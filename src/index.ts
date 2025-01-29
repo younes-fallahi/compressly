7f4d015253722f80e757b6da6410bd5c9e72e607
Younes Fallahi <younesfallahi.js@yahoo.com>
2025-01-29 10:37:04 +0330

    Add logging setup with winston

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { startBot } from "./bot";
+import logger from "./utils/logger";
 
 async function main() {
   try {
     await startBot();
-    console.log("bot is running...");
-  } catch (err) {
-    console.log(err);
+    logger.info("bot has been launched !");
+  } catch (error) {
+    logger.error(`Application failed to start: ${(error as Error).message}`);
   }
 }
 main();
